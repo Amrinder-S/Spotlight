169046fac80d29ffe99d477b9b09a0e4d12c7c34
AMR <amrSAMP@proton.me>
2024-02-27 00:36:28 +0530

    fixed some stuff

---
+++ Spotlight/server/db.js
@@ -0,0 +1,30 @@
+const { Pool } = require('pg');
+
+async function getDatabasePool() {
+    const pool = new Pool({
+        user: 'postgres',
+        host: 'amr.sytes.net',
+        database: 'buspass',
+        password: 'Raghav6969Jit42O',
+        port: 7070,
+    });
+
+    return pool;
+}
+    
+// Example query
+// (async () => {
+
+//     const poool = await getDatabasePool();
+//     poool.query('SELECT * FROM test', (err, res) => {
+//         if (err) {
+//             console.error('Error executing query', err.stack);
+//         } else {
+//             console.log('Connected to PostgreSQL at', res.rows);
+//         }
+//     });
+// })();
+
+module.exports = {
+    getDatabasePool
+};
--- Spotlight/server/server.js
+++ Spotlight/server/server.js
@@ -1,87 +1,60 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose')
-const UserModel = require('./user.model');
 const { redirect } = require('react-router-dom');
 const app = express();
+const {getDatabasePool} = require('./db.js');
 
 app.use(cors());
 app.use(express.json());
 
-require('dotenv').config();
-const uri = process.env.CONNECTION_URL;
-mongoose.connect(uri)
-.then(()=>{
-    console.log("Connected successfully")
-
-}).catch(err =>{
-    console.log(err,"No Connection")
-})
-
-
-
-  // Save the user to the database
 
 app.get('/message', (req, res) => {
     res.json({ message: "Search Projects From Database" });
 });
 
 app.post('/login', async (req, res) => {
-    // Access form data from the request body
     const loginData = req.body;
-    let userexist = await UserModel.findOne({email:loginData.email,password:loginData.password});
-    if(!userexist){
-        res.send({
-            message : "Invalid Email or Password",
-            type : "error"
-        })
-    }
-    // Handle the form data as needed
+    const username = 'user@ekus.com';
+    const password = '123';
+    const pool = await getDatabasePool();
+    pool.query(`SELECT * FROM users WHERE username='${username}' AND password='${password}'`, (err, res) => {
+        if (err) {
+            console.error('Error executing query', err.stack);
+        } else {
+            console.log('Connected to PostgreSQL at', res.rows);
+        }
+    });
     console.log(loginData);
   
-    // Send a response
     res.send('Form data received successfully!');
   });
 
 
   app.post('/signup', async (req, res) => {
     try {
-        // Access form data from the request body
         const signupData = req.body;
-        
-        let userexist = await UserModel.findOne({email:signupData.email});
-
-        if( userexist){
-
+        const userexist = false;
+        if(userexist){
         return res.send({
             message : "User Already Exists, Please Create A New Account ",
             type : "error"
         })
     }
-        
+        // const newUser = new UserModel({
+        //     name: signupData.name,
+        //     email: signupData.email,
+        //     password: signupData.password
+        // });
 
-
-        // Create a new user instance
-        const newUser = new UserModel({
-            name: signupData.name,
-            email: signupData.email,
-            password: signupData.password
-        });
-
-        // Save the user to the database using await
-        await newUser.save();
-
-        // Send a success response
         res.send({
             message : "Congratulations, Account Created Successfully!",
             type : "success"
         });
        
     } catch (error) {
-        // Handle the error, e.g., log it
         console.error('Error saving user:', error);
 
-        // Send an error response
         res.status(500).send('Internal Server Error');
     }
 });
