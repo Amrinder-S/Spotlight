de2229c3c12dc1ea85402167b2161527b851c1ab
Ekuspreet <ekuspreetsingh2@gmail.com>
2024-02-28 22:55:01 +0530

    added dialogue box

--- Spotlight/src/Pages/Profile.jsx
+++ Spotlight/src/Pages/Profile.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import React , {useState} from 'react'
 import NavbarSpecific from '../Components/Navbar/NavbarSpecific'
-import { useLocation } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
+import Dialogue from '../Components/Alerts/Dialogue';
 const Profile = () => {
- const location = useLocation()
- const {user} = location.state
+//  const location = useLocation()
+//  const {user} = location.state
+ const [message,setMessage] = useState({
+  display : "visible",
+  message : "This is an alert",
+})
 
+ function handleReject(){
+  alert("Handle rejection")  
+  setMessage({
+      ...message,
+      display : "hidden"
+    })
+ }
+
+ function handleApprove(){
+  alert("Handle Approval")
+  setMessage({
+    ...message,
+    display : "hidden"
+  })
+ }
   return (
     <>
       
     <NavbarSpecific
     backmessage={"Log Out"}
-    pagename={`Welcome  ${user.name}`}/>
+    pagename={`Welcome  Guest`}/>
+
+    <Dialogue 
+      alertmessage = {message.message}
+      btnclose = {handleReject}
+      btnaccept = {handleApprove}
+      visibility = {message.display}
+    />
 
     
     </>
